d364fe125b005a1543aa55c82d671c853ebdcbff
AndreaBorboa <andrea.borboar@potros.itson.edu.mx>
2022-03-29 10:52:05 -0700

    se agrego BD

--- app/src/main/java/andrea/borboa/trajetasd/MainActivity.kt
+++ app/src/main/java/andrea/borboa/trajetasd/MainActivity.kt
@@ -9,11 +9,13 @@ import androidx.core.content.ContextCompat
 import android.Manifest
 import android.widget.Toast
 import java.io.*
+import android.database.sqlite.SQLiteDatabase
 
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    lateinit var amigosDBHelper: miSQLiteHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,8 +43,36 @@ class MainActivity : AppCompatActivity() {
                 123
             )
             binding.btGuardar.setOnClickListener {
-                Guardar(binding.etNuevoDato.text.toString())
-                binding.tvContenido.text = Cargar()
+                if(binding.etNombre.text.isNotBlank() &&
+                            binding.etEmail.text.isNotBlank()){
+                    amigosDBHelper.anyadirDato(binding.etNombre.text.toString(),
+                    binding.etEmail.text.toString())
+                    binding.etNombre.text.clear()
+                    binding.etEmail.text.clear()
+                    Toast.makeText(this,"Guardado",
+                    Toast.LENGTH_SHORT).show()
+                }
+                else
+                    Toast.makeText(this,"No se ha podido guardar",
+                        Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.btConsultar.setOnClickListener{
+            binding.tvConsulta.text=""
+            val db : SQLiteDatabase = amigosDBHelper.readableDatabase
+            val cursor = db.rawQuery(
+                "SELECT * FROM amigos",
+                null)
+            if(cursor.moveToFirst()){
+                do{
+                    binding.tvConsulta.append(
+                        cursor.getInt(0).toString()+ ": ")
+                    binding.tvConsulta.append(
+                        cursor.getString(1).toString()+ ": ")
+                    binding.tvConsulta.append(
+                        cursor.getString(2).toString()+ "\n")
+                } while (cursor.moveToNext())
             }
         }
     }
@@ -62,6 +92,7 @@ class MainActivity : AppCompatActivity() {
                 Toast.LENGTH_LONG).show()
         }
     }
+
 
     fun Cargar() : String {
         var texto = ""
